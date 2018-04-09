43451a300f612a0c81f4f0d2d30edd321e2b4e19
Nick Mah <nickjmah@gmail.com>
2018-04-09 14:50:15 -0700

    Put signatures in lcd.c

--- Assgn3/lcd.c
+++ Assgn3/lcd.c
@@ -5,6 +5,53 @@
  *      Author: Nick
  */
 
+void writeCommand(uint8_t cmd)
+{
+    //sends a command to the lcd
+}
+void clearDisplay()
+{
 
+}
+void returnHome()
+{
+
+}
+void setEntryMode(uint32_t direction, uint32_t dispShift)
+{
+
+}
+void dispOnSet(uint32_t disp, uint32_t cursor, uint32_t cursorBlink)
+{
+
+}
+void shift(uint32_t cursorShift, uint32_t direction)
+{
+
+}
+void funcSet(uint32_t dataLen, uint32_t numLines, uint32_t font)
+{
+
+}
+void setCGRAM(uint32_t address)
+{
+
+}
+void setDDRAM(uint32_t address)
+{
+
+}
+uint32_t checkBusy()
+{
+
+}
+void writeData(uint32_t data)
+{
+
+}
+uint32_t readData()
+{
+
+}
 
 
