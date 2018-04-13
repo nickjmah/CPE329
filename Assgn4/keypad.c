86d256bcae08e5b7c9f0f4943158be3a7de87c8f
Jason Zhou <jzhou15@calpoly.edu>
2018-04-13 14:46:02 -0700

    Added back keypad.c and keypad.h

---
+++ Assgn4/keypad.c
@@ -0,0 +1,18 @@
+/*
+ * keypad.c
+ *
+ *  Created on: Apr 13, 2018
+ *      Author: Jason Zhou
+ */
+#include "keypad.h"
+
+void key_init(){
+    COL_STRUCT->DIR &= ~(C0|C1|C2);
+
+
+}
+
+void key_loop(){
+
+}
+
