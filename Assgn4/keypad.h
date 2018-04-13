5c3f5208c50edcb4cc23c5a7658bc242a2bf86df
Nick Mah <nickjmah@gmail.com>
2018-04-13 14:59:53 -0700

    changed header to be how I think the code should be structured

--- Assgn4/keypad.h
+++ Assgn4/keypad.h
@@ -4,10 +4,12 @@
  *  Created on: Apr 13, 2018
  *      Author: Jason Zhou
  */
+#include "msp.h"
+
+
 
 #ifndef KEYPAD_H_
 #define KEYPAD_H_
-#include "msp.h"
 #define COL_STRUCT P4
 #define C0 BIT0
 #define C1 BIT1
@@ -17,9 +19,21 @@
 #define R1 BIT5
 #define R2 BIT6
 #define R3 BIT7
-
+#define ONE BIT0
+#define TWO BIT1
+#define THREE BIT2
+#define FOUR BIT3
+#define FIVE BIT4
+#define SIX BIT5
+#define SEVEN BIT6
+#define EIGHT BIT7
+#define NINE BIT8
+#define STAR BIT9
+#define ZERO BIT10
+#define POUND BIT11
 void key_init();
-uint8_t checkRow(uint8_t );
+uint16_t checkKP();//returns a 16 bit integer stuffed with booleans representing each key that's pressed
+uint8_t checkRow(uint8_t col);//returns 3 bits of bools of all col numbers in a row
 
 
 
