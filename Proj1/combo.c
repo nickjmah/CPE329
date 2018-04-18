0d1aba2081dda8ff9dea9dfd5531c67e63c9d49b
Nick Mah <nickjmah@gmail.com>
2018-04-17 21:47:08 -0700

    Added in Combo File

---
+++ Proj1/combo.c
@@ -0,0 +1,10 @@
+/*
+ * combo.c
+ *
+ *  Created on: Apr 17, 2018
+ *      Author: Nick
+ */
+
+
+
+
---
+++ Proj1/combo.h
@@ -0,0 +1,15 @@
+/*
+ * combo.h
+ *
+ *  Created on: Apr 17, 2018
+ *      Author: Nick
+ */
+
+#ifndef COMBO_H_
+#define COMBO_H_
+
+#define WOMBO_COMBO '*'
+
+
+
+#endif /* COMBO_H_ */
