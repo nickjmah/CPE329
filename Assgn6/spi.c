9561b4a3ede3b4796f996c0be9015b7dcdcec5bf
Jason Zhou <jzhou15@calpoly.edu>
2018-04-25 16:29:39 -0700

    Added clockDivide function and added some stuff to init

--- Assgn6/spi.c
+++ Assgn6/spi.c
@@ -6,21 +6,26 @@
  */
 #include "spi.h"
 
-void initSpi(uint8_t* sclk, uint8_t* mosi, uint8_t* miso, uint32_t cpol, uint32_t cph){
+void initSpi(uint16_t baud){
     P1->SEL1 &= ~SPI_MASK;
     P1->SEL0 |= SPI_MASK;
 
-    EUSCI_B0->CTLW0 |= EUSCI_B_CTLW0_SWRST; //reset universal serial commmunication interface
-    EUSCI_B0->CTLW0 = EUSCI_B_CTLW0_SWRST|EUSCI_B_CTLW0_MST|EUSCI_B_CTLW0_SYNC|
-                      EUSCI_B_CTLW0_CPL|EUSCI_B_CTLW0_UCSSEL_2|EUSCI_B_CTLW0_MSB;
-    EUSCI_B0->BRW = ;//use sysfreq or sysfreq function here
+    EUSCI_B0->CTLW0 |= EUSCI_B_CTLW0_SWRST;     //reset universal serial commmunication interface
+    EUSCI_B0->CTLW0 = EUSCI_B_CTLW0_SWRST|      //reset again?
+                      EUSCI_B_CTLW0_MST|        //set MSP as master
+                      EUSCI_B_CTLW0_SYNC|       //enable synchronous communication
+                      EUSCI_B_CTLW0_CKPL|       //inactive state is low
+                      EUSCI_B_CTLW0_UCSSEL_2|   //select SMCLK
+                      EUSCI_B_CTLW0_MSB;        //MSB is received/transmitted first
+    EUSCI_B0->BRW = clockDivide(baud); //use sysfreq or sysfreq function here
     EUSCI_B0->CTLW0 &= ~EUSCI_B_CTLW0_SWRST; //turn on eUSCI state machine
     EUSCI_B0->IE |= EUSCI_B_IE_RXIE; //enable interrupts to be received
-    NVIC->ISER[0] |= 1<<((EUSCI_B0_IRQn) & 31);
-
+    NVIC->ISER[0] |= 1<<((EUSCIB0_IRQn) & 31);
 }
 
-
+uint32_t clockDivide(uint16_t baud){
+    return sysFreq / baud;
+}
 
 
 
