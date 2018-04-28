f7307f7acb60167d4395ec5fcaaa505c751dbc3a
Jason Zhou <jzhou15@calpoly.edu>
2018-04-27 21:00:17 -0700

    Added comments for dac.c and spi.c

--- Assgn6/dac.c
+++ Assgn6/dac.c
@@ -1,23 +1,25 @@
-/*
- * dac.c
+/** \file dac.c
+ * \brief DAC data processing and transmission
  *
- *  Created on: Apr 25, 2018
- *      Author: Nick
+ * \author Nick Mah
+ * \author Jason Zhou
+ *
  */
 #include "dac.h"
 void dacOut(uint16_t voltage)
 {
-    uint8_t bytePacket[2] = {};
-    uint16_t dataByte = voltage | GA | SHDN | BUF;
-    bytePacket[0] = (dataByte>>8);
-    bytePacket[1] = dataByte & (0x00ff);
-    sendData(bytePacket, sizeof(bytePacket));
+    uint8_t bytePacket[2] = {}; //initializing uint8_t array carrying two bytes of data
+    uint16_t dataByte = voltage | GA | SHDN | BUF; //masking voltage input value with output
+                                                   //gain, shutdown, and buffer bits
+    bytePacket[0] = (dataByte>>8); //setting first entry of array as the upper byte
+    bytePacket[1] = dataByte & (0x00ff); //setting second entry as the lower byte
+    sendData(bytePacket, sizeof(bytePacket)); //sending data array through SPI command
     return;
 }
 
 uint16_t voltageOut(float desiredV)
 {
-    return desiredV * 4095 / 3.3;
+    return desiredV * 4095 / 3.3; //converting floating point voltage to DAC input bits
 }
 
 
--- Assgn6/spi.c
+++ Assgn6/spi.c
@@ -11,13 +11,14 @@ volatile uint8_t RXDATA;
 
 void initSpi(uint16_t baud)
 {
-    SPI_STRUCT->SEL1 &= ~SPI_MASK;
-    SPI_STRUCT->SEL0 |= SPI_MASK;
-    CS_STRUCT->DIR |= CS0;
-    CS_STRUCT->OUT |= CS0;
+    SPI_STRUCT->SEL1 &= ~SPI_MASK; //resetting port 1 select bits
+    SPI_STRUCT->SEL0 |= SPI_MASK; //setting port 0 select bits
+    CS_STRUCT->DIR |= CS0; //setting chip select direction
+    CS_STRUCT->OUT |= CS0; //setting chip select high
 
-    EUSCI_B0->CTLW0 |= EUSCI_B_CTLW0_SWRST;     //reset universal serial commmunication interface
-    EUSCI_B0->CTLW0 = EUSCI_B_CTLW0_SWRST|      //reset again?
+    EUSCI_B0->CTLW0 |= EUSCI_B_CTLW0_SWRST;     //reset universal serial
+                                                //commmunication interface
+    EUSCI_B0->CTLW0 = EUSCI_B_CTLW0_SWRST|
                       EUSCI_B_CTLW0_MST|        //set MSP as master
                       EUSCI_B_CTLW0_SYNC|       //enable synchronous communication
                       EUSCI_B_CTLW0_CKPL|       //inactive state is low
@@ -26,37 +27,41 @@ void initSpi(uint16_t baud)
     EUSCI_B0->BRW = clockDivide(baud); //use sysfreq or sysfreq function here
     EUSCI_B0->CTLW0 &= ~EUSCI_B_CTLW0_SWRST; //turn on eUSCI state machine
     EUSCI_B0->IE = EUSCI_B_IE_RXIE; //enable interrupts to be received
-    NVIC->ISER[0] = 1<<((EUSCIB0_IRQn) & 31);
+    NVIC->ISER[0] = 1<<((EUSCIB0_IRQn) & 31); //enable NVIC interrupts
 }
 
 uint32_t clockDivide(uint16_t baud){
-    return sysFreq / baud;
+    return sysFreq / baud; //setting clock division by dividing system frequency
+                           //by desired baud rate
 }
 
 void sendData(uint8_t* data, size_t size){
-    CS_STRUCT->OUT &= ~CS0;
-    int i=0;
-    for(i = 0; i < size; i++){
-        while(!(EUSCI_B0->IFG & EUSCI_B_IFG_TXIFG)){
-            asm("");
+    CS_STRUCT->OUT &= ~CS0; //reset chip select low to start data transmission
+    int i=0; //initialize counter to zero
+    for(i = 0; i < size; i++){ //loop for size of array
+        while(!(EUSCI_B0->IFG & EUSCI_B_IFG_TXIFG)){ //no op for when interrupt flag is
+                                                     //not raised
+            asm(""); //prevent while loop from being compiled out at higher optimizations
         }
-        EUSCI_B0->TXBUF = *data;
-        data++;
+        EUSCI_B0->TXBUF = *data; //set TXbuffer to one byte of data
+        data++; //increment data byte pointer
     }
-    CS_STRUCT->OUT |= CS0;
+    CS_STRUCT->OUT |= CS0; //set chip select high to end data transmission
 }
 
 void EUSCIB0_IRQHandler(void){
-    if(checkSPIReadFlag()){
-        RXDATA = EUSCI_B0->RXBUF;
+    if(checkSPIReadFlag()){ //check SPI read flag
+        RXDATA = EUSCI_B0->RXBUF; //set volatile RXDATA variable to contents of RXBUF
+                                  //also resets EUSCI_BO RX interrupt flag
     }
 }
 
 uint8_t readSPI(void){
-    return RXDATA;
+    return RXDATA; //read back volatile RXDATA variable
 }
 
 uint32_t checkSPIReadFlag(void){
-    return EUSCI_B0->IFG & EUSCI_B_IFG_RXIFG;
+    return EUSCI_B0->IFG & EUSCI_B_IFG_RXIFG; //return 1 or 0 if EUSCI_B0 RX interrupt
+                                              //flag was raised
 }
 
