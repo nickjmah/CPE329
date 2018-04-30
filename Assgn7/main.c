bc9490fcfbf9bf5e509aeaa6ec22d7797abb09be
Nick Mah <nickjmah@gmail.com>
2018-04-29 17:20:48 -0700

    Pasted in boilerplate code

--- Assgn7/main.c
+++ Assgn7/main.c
@@ -1,10 +1,44 @@
-#include "msp.h"
-
-
 /**
  * main.c
  */
-void main(void)
+#include "msp.h"
+#include <math.h>
+var_type TestFunction(var_type num);
+int main(void)
 {
-	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
+    var_type mainVar;
+    WDTCTL = WDTPW | WDTHOLD;
+    // Stop watchdog timer
+    P1->SEL1 &= ~BIT0;
+    //set P1.0 as simple I/O
+    P1->SEL0 &= ~BIT0;
+    P1->DIR |= BIT0;
+    //set P1.0 as output
+    P2->SEL1 &= ~(BIT2 | BIT1 | BIT0);
+    //set P2.0-2.2 as simple I/O
+    P2->SEL0 &= ~(BIT2 | BIT1 | BIT0);
+    P2->DIR |= (BIT2 | BIT1 | BIT0);
+    //set P2.0-2.2 as output pins
+    P2->OUT |= (BIT2 | BIT1 | BIT0);
+    // turn on RGB LED
+    mainVar = T
+    estFunction(15);
+    // test function for timing
+    P2->OUT &= ~(BIT2 | BIT1 | BIT0);
+    // turn off RGB LED
+    while (1)
+        // infinite loop to do nothing
+        mainVar++;
+        // increment mainVar to eliminate not used warning
+}
+var_type TestFunction(var_type num)
+{
+    var_type testVar;
+    P1->OUT |= BIT0;
+    // set P1.0 LED on
+    {
+    insert_function_here (ie testVar = num;)}
+    P1->OUT &= ~BIT0;
+    // set P1.0 LED off
+    return testVar;
 }
