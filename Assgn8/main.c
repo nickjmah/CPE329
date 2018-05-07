3a5f39626f6e1d9e1d19ea2b6b1e1a34bba1941f
Jason Zhou <jzhou15@calpoly.edu>
2018-05-07 14:46:11 -0700

    UART MCLK and SMLK both able to run at 48MHz

--- Assgn8/main.c
+++ Assgn8/main.c
@@ -8,27 +8,55 @@
  */
 
 ///Setting global MCLK frequency
-uint32_t sysFreq = FREQ_3000_KHZ;
+uint32_t sysFreq = FREQ_48000_KHZ;
 
 void main(void)
 {
 	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
 	set_DCO(sysFreq);
-	P1->SEL0 |= BIT2|BIT3;
-	EUSCI_A0->CTLW0 |= EUSCI_A_CTLW0_SWRST;
-	EUSCI_A0->CTLW0 = EUSCI_A_CTLW0_SWRST |
-	                    EUSCI_A_CTLW0_SSEL__SMCLK;
-	EUSCI_A0->BRW = 1;
-	EUSCI_A0->MCTLW = 10<<EUSCI_A_MCTLW_BRF_OFS |
-	                    EUSCI_A_MCTLW_OS16;
-	NVIC->ISER[0] = 1<<((EUSCIA0_IRQn)&31);
-	__enable_irq();
+	// Configure UART pins
+    P1->SEL0 |= BIT2 | BIT3;                // set 2-UART pin as secondary function
 
+    // Configure UART
+    EUSCI_A0->CTLW0 |= EUSCI_A_CTLW0_SWRST; // Put eUSCI in reset
+    EUSCI_A0->CTLW0 = EUSCI_A_CTLW0_SWRST | // Remain eUSCI in reset
+            EUSCI_B_CTLW0_SSEL__SMCLK;      // Configure eUSCI clock source for SMCLK
+    // Baud Rate calculation
+    // 48000000/(16*115200) = 26.0417
+    // Fractional portion = 0.0417
+    // User's Guide Table 21-4: UCBRSx = 0xAD
+    // UCBRFx = int ( (26.0417 - 26)*16) = 0
+    EUSCI_A0->BRW = 26;                     // 48000000/16/115200
+    EUSCI_A0->MCTLW = (0 << EUSCI_A_MCTLW_BRF_OFS) |
+            EUSCI_A_MCTLW_OS16;
+
+    EUSCI_A0->CTLW0 &= ~EUSCI_A_CTLW0_SWRST; // Initialize eUSCI
+    EUSCI_A0->IFG &= ~EUSCI_A_IFG_RXIFG;    // Clear eUSCI RX interrupt flag
+    EUSCI_A0->IE |= EUSCI_A_IE_RXIE;        // Enable USCI_A0 RX interrupt
+
+    // Enable sleep on exit from ISR
+    SCB->SCR |= SCB_SCR_SLEEPONEXIT_Msk;
+
+    // Enable global interrupt
+    __enable_irq();
+
+    // Enable eUSCIA0 interrupt in NVIC module
+    NVIC->ISER[0] = 1 << ((EUSCIA0_IRQn) & 31);
+
+    // Enter LPM0
+    __sleep();
+    __no_operation();                       // For debugger
 }
 
-void EUSCI_A0_IRQHandler(void) {
-    if(EUSCI_A0->IFG & EUSCI_A_IFG_RXIFG){
+// UART interrupt service routine
+void EUSCIA0_IRQHandler(void)
+{
+    if (EUSCI_A0->IFG & EUSCI_A_IFG_RXIFG)
+    {
+        // Check if the TX buffer is empty first
         while(!(EUSCI_A0->IFG & EUSCI_A_IFG_TXIFG));
+
+        // Echo the received character back
         EUSCI_A0->TXBUF = EUSCI_A0->RXBUF;
     }
 }
