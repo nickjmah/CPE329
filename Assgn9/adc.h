ef592005dfa52cf2b37df0e6a4aadabbfa10e786
Nick Mah <nickjmah@gmail.com>
2018-05-10 19:02:30 -0700

    Made headers that I think are necessary for operation of ADC

--- Assgn9/adc.h
+++ Assgn9/adc.h
@@ -7,9 +7,11 @@
 
 #ifndef ADC_H_
 #define ADC_H_
+#include <stdint.h>
 
-
-
-
+void initADC(void);
+uint16_t readADCFlag(void);
+uint16_t readADC(void);
+void ADC14_IRQHandler(void);
 
 #endif /* ADC_H_ */
