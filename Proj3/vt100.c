c515f7c2f6cc3f43c3c702dd6964f1731e6aa1a0
Nick Mah <nickjmah@gmail.com>
2018-05-16 17:13:15 -0700

    Completed terminal set up
    Should be decentely easy to change

--- Proj3/vt100.c
+++ Proj3/vt100.c
@@ -6,35 +6,141 @@
  */
 
 #include "vt100.h"
-char* barGraph(uint32_t minVal, uint32_t maxVal, uint32_t val, uint32_t xPos, uint32_t yPos, char* title)
+
+void initGUI(void)
+{
+    sendUARTString(
+            "+--------------------------------------------------------+\r\n"
+            "|                       CPE329: DMM                      |\r\n"
+            "+--------------------------------------------------------+\r\n"
+            "|Voltage  (VDC):                                         |\r\n"
+            "|Voltage  (VAC):                                         |\r\n"
+            "|Frequency (Hz):                                         |\r\n"
+            "|Peak-Peak  (V):                                         |\r\n"
+            "|RMS     (Vrms):                                         |\r\n"
+            "|                                                        |\r\n"
+            "|                                                        |\r\n"
+            "|                                                        |\r\n"
+            "|                                                        |\r\n"
+            "|                                                        |\r\n"
+            "|                                                        |\r\n"
+            "+--------------------------------------------------------+");
+    //init bar graphs as empty
+    barGraph(0, V_RMS_X_POS, V_RMS_Y_POS, "Vrms");
+    barGraph(0, V_DC_X_POS, V_DC_Y_POS, "Vdc");
+    updateAll(DMM_MIN_VAL, DMM_MIN_VAL, DMM_MIN_VAL, DMM_MIN_VAL, DMM_MIN_VAL);
+    //draw title
+}
+void updateVDC(uint32_t val)
+{
+    static char measBuf[MEAS_BUFFER_SIZE]; //the buffer that to generate the string
+    sprintf(measBuf, "\033[%d;%dH %s", MEAS_START_YPNT, MEAS_START_XPNT,
+            itoaADC(val));
+    sendUARTString(measBuf);
+}
+
+void updateVAC(uint32_t val)
+{
+    static char measBuf[MEAS_BUFFER_SIZE]; //the buffer that to generate the string
+    sprintf(measBuf, "\033[%d;%dH %s", MEAS_START_YPNT + VAC_YPOS,
+    MEAS_START_XPNT,
+            itoaADC(val));
+    sendUARTString(measBuf);
+}
+
+void updateFreq(uint32_t val)
+{
+    static char measBuf[MEAS_BUFFER_SIZE]; //the buffer that to generate the string
+    sprintf(measBuf, "\033[%d;%dH %s", MEAS_START_YPNT + FREQ_YPOS,
+    MEAS_START_XPNT,
+            itoaADC(val));//TODO: change when freq format decided
+    sendUARTString(measBuf);
+}
+
+void updatePkPk(uint32_t val)
+{
+    static char measBuf[MEAS_BUFFER_SIZE]; //the buffer that to generate the string
+    sprintf(measBuf, "\033[%d;%dH %s", MEAS_START_YPNT + PK_PK_YPOS,
+    MEAS_START_XPNT,
+            itoaADC(val));
+    sendUARTString(measBuf);
+}
+
+void updateRMS(uint32_t val)
+{
+    static char measBuf[MEAS_BUFFER_SIZE]; //the buffer that to generate the string
+    sprintf(measBuf, "\033[%d;%dH %s", MEAS_START_YPNT + RMS_YPOS,
+    MEAS_START_XPNT,
+            itoaADC(val));
+    sendUARTString(measBuf);
+}
+void updateAll(uint32_t vdc, uint32_t vac, uint32_t freq, uint32_t pkPk,
+               uint32_t rms)
+{
+    updateVDC(vdc);
+    updateVAC(vac);
+    updateFreq(freq);
+    updatePkPk(pkPk);
+    updateRMS(rms);
+}
+char* barGraph(uint32_t val, uint32_t xPos, uint32_t yPos, char* title)
 {
     static char buf[BUFFER_SIZE]; //the buffer that to generate the string
-    uint32_t fillAmt = (MAX_PERCENT*val)/(maxVal)-minVal;
-    int n=0;
-    int i=0;
+    uint32_t fillAmt = (MAX_PERCENT * val) / (DMM_MAX_VAL) - DMM_MIN_VAL;
+    int n = 0;
+    int i = 0;
     char barVal = '-';
-    n = sprintf(buf,"\033[%d;%dH %s", yPos+1, xPos, title);
+    n = sprintf(buf, "\033[%d;%dH %s", yPos + 1, xPos, title);
     sendUARTString(buf);
-    n = sprintf(buf,"\033[%d;%dH %dV", yPos, xPos-3, minVal);
+    n = sprintf(buf, "\033[%d;%dH 0V", yPos, xPos - 3);
     sendUARTString(buf);
-    n = sprintf(buf,"\033[%d;%dH %dV", yPos-BAR_HEIGHT+1, xPos-5, maxVal);
+    n = sprintf(buf, "\033[%d;%dH 3V", yPos - BAR_HEIGHT + 1, xPos - 3);
     sendUARTString(buf);
-    for(i=0;i<BAR_HEIGHT;i++)
+    for (i = 0; i < BAR_HEIGHT; i++)
     {
-        fillAmt -=BAR_RES;
-        if(fillAmt<MAX_PERCENT)
+        fillAmt -= BAR_RES
+        ;
+        if (fillAmt < MAX_PERCENT)
         {
             barVal = '#';
 
         }
         else
             barVal = '-';
-        n = sprintf(buf,"\033[%d;%dH %c %d", yPos-i, xPos, barVal,fillAmt);
-        if(n<BUFFER_SIZE)
+        n = sprintf(buf, "\033[%d;%dH %c", yPos - i, xPos, barVal);
+        if (n < BUFFER_SIZE)
             sendUARTString(buf);
         else
             sendUARTString("Uh Oh");
     }
 
     return buf;
+}
+
+char* itoaADC(uint32_t val)
+{
+    int adc = val * 33 / 10;
+    static char buf[32] = { 0 };
+    int i, j = 0;
+    for (i = 0; i < SIG_FIGS - 1; i++)
+    {
+        adc *= 10;
+    }
+    adc /= ADC_MAX_VAL;
+    i = 30;
+    buf[i] = 'V';
+    i--;
+    for (j = 0; j < SIG_FIGS + 1; i--, j++)
+    {
+        if (j == SIG_FIGS - 1)
+        {
+            buf[i] = '.';
+        }
+        else
+        {
+            buf[i] = "0123456789"[(adc) % 10];
+            adc /= 10;
+        }
+    }
+    return &buf[i + 1];
 }
--- Proj3/vt100.h
+++ Proj3/vt100.h
@@ -7,13 +7,36 @@
 #include "stdint.h"
 #include "stdio.h"
 #include "uart.h"
-#define BAR_HEIGHT 10 //bar height in terms of characters
-#define BUFFER_SIZE 100
-#define MAX_PERCENT 100
-#define BAR_RES MAX_PERCENT / BAR_HEIGHT;
+#include "adc.h"
+
 #ifndef VT100_H_
 #define VT100_H_
 
+#define BAR_HEIGHT 10 //bar height in terms of characters
+#define BUFFER_SIZE 100
+#define MEAS_BUFFER_SIZE 10
+#define MAX_PERCENT 100
+#define BAR_RES MAX_PERCENT / BAR_HEIGHT;
+#define DMM_MAX_VAL 14895 // 3 / 3.3 * 2^14
+#define DMM_MIN_VAL 0
+#define V_RMS_Y_POS 13
+#define V_RMS_X_POS 52
+#define V_DC_Y_POS 13
+#define V_DC_X_POS 35
+#define MEAS_START_XPNT 17
+#define MEAS_START_YPNT 4
+#define VAC_YPOS 1
+#define FREQ_YPOS 2
+#define PK_PK_YPOS 3
+#define RMS_YPOS 4
+#define SIG_FIGS 4
+/** \brief initalizes header for the GUI
+ *
+ */
+void initGUI(void);
+/** \brief updates GUI
+ *
+ */
 /** \brief draws a barGraph at a spot on the VT100 terminal
  *
  *  \param minVal   The minimum value of the input data
@@ -24,7 +47,16 @@
  *  \param title    The title for the graph
  *  \return pointer to the buffer holding the string
  */
-char* barGraph(uint32_t minVal, uint32_t maxVal, uint32_t val,
-               uint32_t xPos, uint32_t yPos, char* title);
-
+char* barGraph(uint32_t val, uint32_t xPos, uint32_t yPos, char* title);
+void updateVDC(uint32_t val);
+void updateVAC(uint32_t val);
+void updateFreq(uint32_t val);
+void updatePkPk(uint32_t val);
+void updateRMS(uint32_t val);
+void updateAll(uint32_t vdc, uint32_t vac, uint32_t freq, uint32_t pkPk,
+               uint32_t rms);
+/** \converts an ADC value to a number in decimal
+ *
+ */
+char* itoaADC(uint32_t val);
 #endif /* VT100_H_ */
