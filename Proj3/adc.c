d9bf4c12275f50bd65a5e59211dd990160ce89ae
Nick Mah <nickjmah@gmail.com>
2018-05-18 21:57:38 -0700

    Commented and refactored ADC

--- Proj3/adc.c
+++ Proj3/adc.c
@@ -17,25 +17,24 @@ static uint8_t ADCFlag=0;
 
 char* itoa(int val)
 {
-    static char buf[ITOA_BUF_SIZE] = {0};
+    static char buf[ITOA_BUF_SIZE] = {0}; //create a static buffer for string output
     int i = ITOA_INDEX_START;
-    for(; val != 0; i--)
+    for(; val != 0; i--)                  //loop until val is 0
     {
-        buf[i] = "0123456789"[(val) % 10];
-        val /= 10;
+        buf[i] = "0123456789"[(val) % 10];//give buf the least significant bit of val
+        val /= 10;                        //decimal shift right val
     }
-    return &buf[i+1];
+    return &buf[i+1];                     //return index of beginning of string
 }
 
 void initADC(void)
 {
     ADC_STRUCT->SEL1 |= ADC_0;                       // Configure P5.4 for ADC
     ADC_STRUCT->SEL0 |= ADC_0;
-    NVIC->ISER[0] = 1 << ((ADC14_IRQn) & 31);
+    NVIC->ISER[0] = 1 << ((ADC14_IRQn) & 31);       //attach interrupt
 
-    // Sampling time, S&H=16, ADC14 on
+    // Sampling time, S&H=4, ADC14 on, multiple sample and conversion
     ADC14->CTL0 = ADC14_CTL0_SHT0_0 | ADC14_CTL0_SHP | ADC14_CTL0_ON | ADC14_CTL0_MSC;
-//                  |ADC14_CTL0_SSEL__SMCLK | ADC14_CTL0_DIV_1 | ADC14_CTL0_PDIV__64;
     ADC14->CTL1 = ADC14_CTL1_RES__14BIT;         // Use sampling timer, 14-bit conversion results
 
     ADC14->MCTL[0] |= ADC14_MCTLN_INCH_1;   // A1 ADC input select; Vref=AVCC
@@ -50,20 +49,20 @@ uint16_t readADCFlag(void)
 uint32_t readADC(void)
 {
     ADCFlag = 0;
-    return ADCMem0 + ADC_CAL; //no filtering
+    return ADCMem0 + ADC_CAL;//adds calibration value to make results more accurate
 }
 
 void startConv(void)
 {
-    ADC14->CTL0 |= ADC14_CTL0_ENC | ADC14_CTL0_SC;
+    ADC14->CTL0 |= ADC14_CTL0_ENC | ADC14_CTL0_SC;//enables a conversion
 }
 
 void ADC14_IRQHandler(void)
 {
     if (ADC14->IFGR0 & ADC14_IFGR0_IFG0)    // check if MEM0 flag is raised
     {
-        ADCMem0 = ADC14->MEM[0];
-        ADCFlag = 1;
+        ADCMem0 = ADC14->MEM[0];//save the MEM[0] register, which is a single result
+        ADCFlag = 1;            //raise ADC read flag
     }
 }
 
--- Proj3/adc.h
+++ Proj3/adc.h
@@ -12,19 +12,49 @@
 
 #define ADC_STRUCT P5
 #define ADC_0 BIT4
-#define ADC_CAL 0
-#define ADC_MAX_VAL 16384
+#define ADC_CAL 0               //DC offset calibration for ADC
+#define ADC_MAX_VAL 16384       //maximum decimal value of the ADC
 #define ADC_MAX_ANALOG_VAL 3300 //maximum voltage for the ADC in mV
 #define UNITS_MILLI 1000
-#define ITOA_BUF_SIZE 32
+#define ITOA_BUF_SIZE 32        //size of the ITOA buffer
 #define ITOA_INDEX_START 30
 
+/** \brief converts a decimal number into a string
+ *  itoa takes in an unsigned integer and converts it into a string representation.
+ *  A static buffer holds the result of the number. The function works like a decimal
+ *  shift register. Val is continuously decimal shifted right and the least significant digit
+ *  is used as the index of a string for the next value of buf. A pointer to the first
+ *  value of buffer is returned and can be used in generic string print function
+ *
+ *  \param val A number to be turned into a string
+ *  \return pointer to the buffer holding the first character of the string
+ */
 char* itoa(int val);
+/** \brief initializes the ADC
+ *  Sets up P5.4 as a 14-bit ADC pin with interrupts. The ADC is run in sample and hold
+ *  mode with multiple sample and conversion. The ADC will be taking a Vref of 3.3V as well
+ *
+ *  \return void
+ */
 void initADC(void);
+/** \brief Sends a command to initialize a conversion
+ *
+ */
 void startConv(void);
+/** \brief reads the local ADC conversion complete flag
+ *
+ */
 uint16_t readADCFlag(void);
+/** \brief reads the value of the ADC stored as a file local global variable
+ *
+ */
 uint32_t readADC(void);
-uint32_t averageADC(uint32_t* arrayADC, uint8_t arraySize);
+/** \brief IRQHandler for the AD
+ *
+ *  This IRQ is called when the measurement is ready. Upon execution, the value will be
+ *  saved to a global variable for later usage and a flag will be rased
+ *
+ */
 void ADC14_IRQHandler(void);
 //void TA0_0_IRQHandler(void);
 
