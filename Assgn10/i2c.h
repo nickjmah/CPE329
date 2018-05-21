8acb7bcec1a6b335cf91b2667d357255f1840c54
Nick Mah <nickjmah@gmail.com>
2018-05-21 13:41:36 -0700

    Added some more functions

--- Assgn10/i2c.h
+++ Assgn10/i2c.h
@@ -9,8 +9,16 @@
 #define I2C_H_
 
 #include "msp.h"
-
+#include "stdint.h"
+#include "stdio.h"
 void initI2C(void);
-
-
+void sendI2C(uint8_t address, size_t size);
+/** \brief reads RxData
+ *
+ *  \param RxData_Ptr pointer to the first element of the array containing RxData
+ *  \return The size of the amount of data sent
+ *
+ */
+uint8_t readI2C(uint8_t* RxData_Ptr)
+uint8_t readI2CReceiveFlag(void);
 #endif /* I2C_H_ */
