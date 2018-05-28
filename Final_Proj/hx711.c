ba9c517afa01c3fc76fae92a192f61b00f550296
Jason Zhou <jzhou15@calpoly.edu>
2018-05-27 17:43:24 -0700

    Added tare function as well as some other functionality

--- Final_Proj/hx711.c
+++ Final_Proj/hx711.c
@@ -7,11 +7,19 @@
 
 #include "hx711.h"
 
+static uint32_t OFFSET = 0;
+static float SCALE = 1;
+
 void initHX711(void)
 {
     HX711_STRUCT->DIR &= ~HX711_DO;     //set data in to input
     HX711_STRUCT->DIR |= HX711_CLK;     //set clock out to output
     HX711_STRUCT->OUT &= ~HX711_CLK;    //clear clock out
+}
+
+void powerUp(void)
+{
+    HX711_STRUCT->OUT &= ~HX711_CLK;
 }
 
 void powerDown(void)
@@ -26,13 +34,13 @@ uint32_t readCount(void)
 //    HX711_STRUCT->IN |= HX711_DO; //this is probably wrong and might not need to be here
     HX711_STRUCT->OUT &= ~HX711_CLK;        //set clock low for normal operation
     count = 0;                              //clear data variable
-    while(HX711_STRUCT->IN && HX711_DO);    //block loop until HX711 is ready to send
+    while(HX711_STRUCT->IN & HX711_DO);    //block loop until HX711 is ready to send
     for(i=0; i<24; i++)                     //define loop to check data with gain of 128
     {
         HX711_STRUCT->OUT |= HX711_CLK;     //set clock high to output data
         count = count << 1;                 //shift count
         HX711_STRUCT->OUT &= ~HX711_CLK;    //set clock low to finish output data instruction
-        if(HX711_STRUCT->IN && HX711_DO)
+        if(HX711_STRUCT->IN & HX711_DO)
             count++;                        //increment data variable if data input was high
     }
     HX711_STRUCT->OUT |= HX711_CLK;         //25th clock cycle to end data transmission
@@ -41,5 +49,49 @@ uint32_t readCount(void)
     return count;
 }
 
+uint32_t readAverage(uint8_t times)
+{
+    uint32_t sum = 0;
+    uint8_t i;
+    for (i = 0; i < times; i++)
+    {
+        sum += readCount();
+    }
+    return sum / times;
+}
 
+void tare(uint8_t times)
+{
+    uint32_t sum = readAverage(times);
+    setOffset(sum);
+}
 
+uint32_t getValue(uint8_t times)
+{
+    return readAverage(times) - OFFSET;
+}
+
+float getUnits(uint8_t times)
+{
+    return getValue(times) / SCALE;
+}
+
+void setScale(float scale)
+{
+    SCALE = scale;
+}
+
+float getScale(void)
+{
+    return SCALE;
+}
+
+void setOffset(uint32_t offset)
+{
+    OFFSET = offset;
+}
+
+uint32_t getOffset(void)
+{
+    return OFFSET;
+}
--- Final_Proj/hx711.h
+++ Final_Proj/hx711.h
@@ -10,13 +10,23 @@
 
 #include "msp.h"
 
-#define HX711_STRUCT P1 //to be changed once hardware is finalized
-#define HX711_CLK 0     //""
-#define HX711_DO 1      //""
+#define HX711_STRUCT P5 //to be changed once hardware is finalized
+#define HX711_CLK BIT4     //""
+#define HX711_DO BIT5      //""
 
 void initHX711(void);
+void powerUp(void);
 void powerDown(void);
 uint32_t readCount(void);
+uint32_t readAverage(uint8_t times);
+uint32_t getValue(uint8_t times);
+float getUnits(uint8_t times);
+void tare(uint8_t times);
+void setScale(float scale);
+float getScale(void);
+void setOffset(uint32_t offset);
+uint32_t getOffset(void);
+
 
 
 #endif /* HX711_H_ */
