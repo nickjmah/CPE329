ef3a625a23554abcc075dde53001a675929c30e3
Jason Zhou <jzhou15@calpoly.edu>
2018-05-30 14:18:04 -0700

    Added some basic comments to the hx711 header file

--- Final_Proj/hx711.h
+++ Final_Proj/hx711.h
@@ -14,17 +14,54 @@
 #define HX711_CLK BIT4     //""
 #define HX711_DO BIT5      //""
 
+/** \brief initializes ports for serial communication with HX711
+ *  Sets up data input and clock output
+ */
 void initHX711(void);
+/** \brief lowers clock output which powers up the HX711
+ *
+ */
 void powerUp(void);
+/** \brief raises clock output which powers down the HX711
+ *
+ */
 void powerDown(void);
+/** \brief reads in clocked bits from the HX711
+ *
+ */
 uint32_t readCount(void);
+/** \brief calls readCount and averages over a number of times
+ *
+ */
 uint32_t readAverage(uint8_t times);
+/** \brief returns readAverage with the offset subtracted
+ *
+ */
 uint32_t getValue(uint8_t times);
+/** \brief returns getValue divided by the scale factor
+ *
+ */
 float getUnits(uint8_t times);
+/** \brief sets the offset by calling readAverage
+ *   Meant to be called with no object on the scale
+ *   The averaged value is then set as the offset, which gets subtracted
+ */
 void tare(uint8_t times);
+/** \brief sets the scale factor
+ *
+ */
 void setScale(float newScale);
+/** \brief returns the scale factor
+ *
+ */
 float getScale(void);
+/** \brief sets the offset value
+ *
+ */
 void setOffset(uint32_t newOffset);
+/** \brief returns the offset value
+ *
+ */
 uint32_t getOffset(void);
 
 
