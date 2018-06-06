08299ea307604bca2637042a50b26055bf168fd3
Jason Zhou <jzhou15@calpoly.edu>
2018-06-06 14:36:58 -0700

    Added some stuff to bitConvertInt

--- Final_Proj/keypad.c
+++ Final_Proj/keypad.c
@@ -76,11 +76,12 @@ char bitConvertChar(uint16_t bitIndex){
 uint32_t bitConvertInt(uint16_t bitIndex){
     //case statement to convert the bits into chars
     switch(bitIndex){
+    case ZERO: return 0;
     case ONE: return 1;
     case TWO: return 2;
     case THREE: return 3;
     case FOUR: return 4;
-    case FIVE: return 4;
+    case FIVE: return 5;
     case SIX: return 6;
     case SEVEN: return 7;
     case EIGHT: return 8;
