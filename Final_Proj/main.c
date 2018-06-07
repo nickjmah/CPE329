6d05a8b9b6a8b0234c2b889bfdecdf7a32d61c6b
Nick Mah <nickjmah@gmail.com>
2018-06-07 14:06:30 -0700

    Commented main. Need to add code to shutoff LCD and maybe some other convenience code

--- Final_Proj/main.c
+++ Final_Proj/main.c
@@ -19,10 +19,13 @@
 uint32_t sysFreq = FREQ_12000_KHZ; //set system frequency to 48MHz
 volatile uint32_t enterSleep = 0; //TODO: change to enum maybe
 volatile uint32_t timerCounter = 0;
+/** \brief helper function to enter sleep mode */
 void sleep(void);
+/** \brief Initializes the all of the unused pins to be inputs to save power */
 void boardInit(void);
+/** \brief Initalizes the sleep timer */
 void initSleepTimer(void);
-void changeMode(uint16_t keyRecorded);
+/** \brief Initializes all peripherals */
 void init(void)
 {
     //initialize all peripherals
@@ -41,7 +44,7 @@ void init(void)
     __DSB();
     __enable_irq();
 }
-
+/** \brief main functions */
 void main(void)
 {
 #ifdef devel
@@ -70,23 +73,23 @@ void main(void)
     }
 
 #elif test
+    /** enum to describe the different states of the state machine */
     typedef enum mode
     {
         weigh, units, height, zero, cal
     } mode_t;
-
-    uint16_t* keyRecorded = 0;
-    uint32_t tmp = 0;
-    float tmp_f = 0;
-    mode_t currentMode = weigh;
+    uint16_t* keyRecorded = 0;//pointer to an array of keypresses
+    uint32_t tmp = 0;//temp variable used for height measurements
+    float tmp_f = 0;//temp variable used for calibration
+    mode_t currentMode = weigh; //sets the inital state to weight
     init();
-    updateScale();
+    updateScale(); //starts off with an initial weight measurement
     while (1)
     {
-        if(enterSleep)//TODO: Reset counters to reenable sleep
+        if(enterSleep)
             sleep();
         //on keypress
-        if (checkPress())
+        if (checkPress())//look to change modes if a keypress occurs
         {
             enterSleep = 0;
             keyRecorded = getKeyArr();
@@ -117,19 +120,20 @@ void main(void)
                 break;
             }
         }
-        //looping
+        //looping mode changes
         switch (currentMode)
         {
         case weigh:
             updateScale();
             break;
+            //update units
         case units:
-            updateUnits();
-            while (!checkPress())
+            updateUnits();//set display to units menu
+            while (!checkPress())//wait until 1 keypress
                 ;
             keyRecorded = getKeyArr();
             switch (*keyRecorded)
-            {
+            {//1 puts you in kg, 2 puts you in lbs
             case ONE :
                 updateSI();
                 break;
@@ -137,48 +141,46 @@ void main(void)
                 updateImp();
                 break;
             default:
-                currentMode = units;
-                break;
+                break;//TODO: check to see if you want to talk about invalid keypresses
             }
             currentMode = weigh;
             break;
         case height:
             updateHeightFt(); //prompt for entering feet
-            while (getArrSize() < 1)
+            while (getArrSize() < 1)//getting height in feet
                 ;
             keyRecorded = getKeyArr();
-            tmp = bitConvertInt(*keyRecorded) * 12;
+            tmp = bitConvertInt(*keyRecorded) * 12;//tmp is in inches so mult by 12
             delay_ms(100, sysFreq);
             updateHeightIn(); //prompt for entering inches
             while (getArrSize() < 2)
                 ;
             keyRecorded = getKeyArr();
-            tmp += bitConvertInt(keyRecorded[0]) * 10;
-            tmp += bitConvertInt(keyRecorded[1]);
-            changeHeight(tmp);
+            tmp += bitConvertInt(keyRecorded[0]) * 10 + bitConvertInt(keyRecorded[1]);//add in inches
+            changeHeight(tmp);//update height
             currentMode = weigh;
-            delay_ms(100, sysFreq);
-            checkPress();
+            delay_ms(100, sysFreq);//add in a delay to see the last typed character
+            checkPress();//clearing the check press since we already cleared the queue
             break;
         case zero:
-            tare(10);
+            tare(10);//tare will zero the scale
             currentMode = weigh;
             break;
         case cal:
             calScreen();
-            while (getArrSize() < 2)
+            while (getArrSize() < 2)//wait for the first 2 keypresses
                 ;
             keyRecorded = getKeyArr();
             tmp_f = bitConvertInt(keyRecorded[0]) * 10
-                    + bitConvertInt(keyRecorded[1]);
-            writeData('.');
-            while (getArrSize() < 2)
+                    + bitConvertInt(keyRecorded[1]);//add the first two presses
+            writeData('.');//print a '.' in order to show that you are now in grams
+            while (getArrSize() < 2)//wait for two presses
                 ;
             keyRecorded = getKeyArr();
             tmp_f += ((float) bitConvertInt(keyRecorded[0])) / 10
-                    + ((float) bitConvertInt(keyRecorded[1])) / 100;
-            calibrate(tmp_f);
-            checkPress();
+                    + ((float) bitConvertInt(keyRecorded[1])) / 100;//convert to float and add in grams
+            calibrate(tmp_f);//run calibration calcs
+            checkPress();//clearing check press
             currentMode = weigh;
             delay_ms(100, sysFreq);
             break;
@@ -228,7 +230,7 @@ void initSleepTimer(void)
     TIMER_A0->CCTL[0] = TIMER_A_CCTLN_CCIE; //enable interrupt
     TIMER_A0->CCR[0] = CCR_INCR;//set initial increment
     //set timer to SMCLK, continuous mode, no prescaler
-    TIMER_A0->CTL = TIMER_A_CTL_SSEL__ACLK |
+    TIMER_A0->CTL = TIMER_A_CTL_SSEL__ACLK |//set timer to ACLK to run even slower
     TIMER_A_CTL_MC__CONTINUOUS | TIMER_A_CTL_ID__8;
 //    TIMER_A0->EX0 |= TIMER_A_EX0_TAIDEX_7;
     NVIC->ISER[0] = 1 << ((TA0_0_IRQn) & 31);//attach interrupt
